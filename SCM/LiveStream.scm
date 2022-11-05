2f6b782354f7eec2bd0811a9ce414c02ffe681e5
LukaUte2019 <46568221+LukaUte2019@users.noreply.github.com>
2022-11-05 13:30:42 +0100

    Add files via upload

---
+++ SCM/LiveStream.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["creator.kodular.io"],"YaVersion":"242","Source":"Form","Properties":{"$Name":"LiveStream","$Type":"Form","$Version":"44","AppId":"6526853399445504","AppName":"Lukabook","MinSdk":"19","PrimaryColor":"&HFF868686","PrimaryColorDark":"&HFF868686","ReceiveSharedText":"video,image,","ShowListsAsJson":"True","Title":"LiveStream","Uuid":"0"}}
+|#
